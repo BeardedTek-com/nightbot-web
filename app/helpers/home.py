e1f9683246b6b7cf70f2cbbf936e8c6f48527d3a
BeardedTek <kenny@beardedtek.com>
2024-07-24 23:28:48 -0800

    again

--- app/helpers/home.py
+++ app/helpers/home.py
@@ -10,13 +10,10 @@ class Home:
         if "NIGHTBOT_BEARER" not in os.environ:
             return "<a href='/oauth/initiate'>LOGIN</a>"
         else:
-            return self.api_list()
-
-    def api_list(self):
-        output = ""
-        for endpoint in api_endpoints:
-            output += f"<a href='{api_endpoints[endpoint]}'>{endpoint}</a></br>"
-        if output == "":
-            return "No Endpoints configured"
-        else:
-            return output
+            output = ""
+            for endpoint in api_endpoints:
+                output += f"<a href='{api_endpoints[endpoint]}'>{endpoint}</a></br>"
+            if output == "":
+                return "No Endpoints configured"
+            else:
+                return output
