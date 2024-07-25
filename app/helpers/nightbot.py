2ae1afb9b3b4cc17a891d30ea3143be6f34add7e
BeardedTek <kenny@beardedtek.com>
2024-07-25 12:47:30 -0800

    again

--- app/helpers/nightbot.py
+++ app/helpers/nightbot.py
@@ -6,35 +6,55 @@ from time import sleep
 
 class NightBot:
     def __init__(self, app_url):
+        ''' NightBot API Helper Class
+            base_url (str): base uri of NightBot's API Server
+            api_base_url: base uri of api
+            authorize_url: url to authenticate via oauth2
+            token_url: url to retrieve oauth2 token
+            redirect_uri: OAUTH2 Redirect URI - MUST BE REGISTERED WITH NIGHTBOT APP
+            scope: scopes we are requesting access to
+        '''
         self.base_url = "https://api.nightbot.tv"
         self.api_base_url = f"{self.base_url}/1"
         self.authorize_path = "/oauth2/authorize"
         self.authorize_url = f"{self.base_url}{self.authorize_path}"
         self.token_path = "/oauth2/token"
         self.token_url = f"{self.base_url}{self.token_path}"
-        self.client_id = "84c9ff8165a03c0b5e7b65a9bb3b7e1e"
-        self.client_secret = "4197b54c7d9b028956a457cef2ffbd4ed4f412f0bd86c718e57997bc91bf939c"
         self.response_type = "code"
-        self.redirect_uri = f"https://nightbot.newtowncrew.com/oauth/token"
-        self.scope = "channel channel_send commands commands_default subscribers timers regulars"
         self.code = None
         self.token = {}
         self.bearer = None
-        if self.client_id != "" and self.client_secret != "":
-            self.ready = True
-        else:
-            self.ready = False
 
+        # DANGER: EVERYTHING BELOW THIS LINE NEEDS TO BE NOT HARD CODED!!! It should be provided by the user
+        self.client_id = "84c9ff8165a03c0b5e7b65a9bb3b7e1e"
+        self.client_secret = "4197b54c7d9b028956a457cef2ffbd4ed4f412f0bd86c718e57997bc91bf939c"
+        self.redirect_uri = f"https://nightbot.newtowncrew.com/oauth/token"
+        self.scope = "channel channel_send commands commands_default subscribers timers regulars"
+
+    def ready(self):
+        ''' Checks to see if 'client_id' and 'client_secret' are provided
+        '''
+        if self.client_id != "" and self.client_secret != "":
+            return True
+        else:
+            return False
+        
     def index(self):
+        ''' Generates the index page
+        '''
         if self.bearer:
             return "Already Authorized"
         else:
             return "<a href='/oauth/initiate'>LOGIN</a>"
         
     def authorize(self):
-        if self.ready:
+        ''' Authenticates with NightBot API via OAUTH2
+        '''
+        if self.ready():
+            ''' If 'client_id' and 'client_secret' are provided we authenticate, otherwise we send the user to /config
+            '''
             print("AUTHORIZE READY")
-            redirect_url = f"{self.authorize_url}?response_type=code&client_id={self.client_id}&redirect_uri={self.redirect_uri}&scope={self.scope}"
+            redirect_url = f"{self.authorize_url}?response_type={self.response_type}&client_id={self.client_id}&redirect_uri={self.redirect_uri}&scope={self.scope}"
             print(redirect_url)
             return redirect( redirect_url, code=302)
         else:
@@ -42,6 +62,11 @@ class NightBot:
             return redirect("/config", code=302)
 
     def get_token(self):
+        ''' Retrieves access_token from NightBot.
+                NOTES:
+                    - Need to store more info about the token including when it will expire, refresh code, etc.
+                    - Should we automatically get a refresh token prior to expiration or require user interaction?
+        '''
         if "code" in request.args:
             self.code = request.args['code']
             data = {
@@ -62,6 +87,11 @@ class NightBot:
         else:
             return jsonify("ERROR: NO TOKEN")
     def get_me(self):
+        ''' NightBot API Endpoint /me: Provides authenticated user information
+                NOTES:
+                    - For now it just returns the JSON provided by the API
+                    - Should display user info in a clear way
+        '''
         if not self.headers:
             return redirect('/', code=302)
         else:
@@ -74,6 +104,8 @@ class NightBot:
             return jsonify(result_json)
     
     def channel_send(self):
+        ''' Sends a test message to make sure things are working
+        '''
         if not self.headers:
             return redirect('/', code=302)
         else:
@@ -89,6 +121,11 @@ class NightBot:
             return jsonify(result_json)
     
     def channel_send_from_file(self,msg):
+        ''' Sends a message stored in a file
+                NOTES:
+                    - Splits messages into 390 character chunks to stay under the 400 character limit.
+                    - Need to refine for different use cases
+        '''
         if not self.headers:
             return redirect('/',code=302)
         else:
@@ -125,9 +162,4 @@ class NightBot:
                         )
                         results.append(json.loads(api_result.text))
                         sleep(5.5)
-                    return jsonify(results)
-
-
-
-
-
+                    return jsonify(results) 
