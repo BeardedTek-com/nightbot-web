656233e21e662168ba848c0eae02fad7414e847f
BeardedTek <kenny@beardedtek.com>
2024-07-30 20:45:01 -0800

    Add POST

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-logs = logging.getlogger('werkzeug')
+logs = logging.getLogger('werkzeug')
 data_flask_log = logging.FileHandler('/data/flask.log')
 logs.addHandler(data_flask_log)
 
