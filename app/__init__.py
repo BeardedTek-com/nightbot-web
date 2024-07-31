0d9a444222096b1f4c719b972d37822566b0e6fb
BeardedTek <kenny@beardedtek.com>
2024-07-30 20:54:11 -0800

    Log to stderr and file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 import logging
 from flask import Flask
+from flask.logging import default_handler
 
 app = Flask(__name__)
 
+# Setup logging
 logs = logging.getLogger('werkzeug')
+# Log to '/data/flask.log'
 data_flask_log = logging.FileHandler('/data/flask.log')
 logs.addHandler(data_flask_log)
+# Log to 'stderr'
+logs.addHandler(default_handler)
+
+
 
 config = {}
 
