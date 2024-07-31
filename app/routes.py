427373513a394edc6694a7ddabfdb8f2e182aa87
BeardedTek <kenny@beardedtek.com>
2024-07-30 20:01:37 -0800

    Add GET's

--- app/routes.py
+++ app/routes.py
@@ -56,7 +56,22 @@ def api_catchall(category,command,cmd_var):
                 nb.channel_send_from_file(cmd_var)
             else:
                 nb.channel_send()
+        elif command == "join":
+            # Join Channel
+            nb.api_send(
+                api.channel_join,
+                param = None,
+                data = None
+            )
+        elif command == "part":
+            # Leave Channel
+            nb.api_send(
+                api.channel_part,
+                param = None,
+                data = None
+            )
         if not command:
+            # Get Channel details
             return nb.api_send(
                 api.channel,
                 param = None,
@@ -65,7 +80,8 @@ def api_catchall(category,command,cmd_var):
     
     # Commands
     if category == "commands":
-        if command == "get":
+        if not command:
+            # Get Custom Commands
             if cmd_var:
                 return nb.api_send(
                     api.custom_command_get,
@@ -75,6 +91,19 @@ def api_catchall(category,command,cmd_var):
             else:
                 return nb.api_send(
                     api.custom_commands_get_all,
+                    data = None
+                )
+        if command == "default":
+            if cmd_var:
+                nb.api_send(
+                    api.default_command_get,
+                    param = cmd_var,
+                    data = None
+                )
+            if not cmd_var:
+                nb.api_send(
+                    api.default_commands_get_all,
+                    param = None,
                     data = None
                 )
     # Me
